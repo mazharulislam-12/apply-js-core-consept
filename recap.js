dc5bf62bce9236221d3cefe6c86234ad7d9013e0
mazharulislam-12 <505mazharulislam@gmail.com>
2023-08-02 11:04:50 -0700

    recap for js

---
+++ recap.js
@@ -0,0 +1,44 @@
+// 1. variable
+ var myName = 'khan';
+
+//  math operation +, -, *, /
+// shorthand +=, -=, *=, /=
+// ++, --
+
+
+// 2. Array
+var friends = ['abul', 'babul', 'kabul', 'ebul']
+var thirdFriends = friends[2];
+friends[3] = 'john deo';
+
+// 3. Conditional
+// <, >, >=, <=, ==, ===, !=, !===
+if (condition) {
+    
+}
+else{
+
+}
+
+// 4. Loop 
+// while loop
+var number = 0;
+while (number <= 6) {
+    console.log(number);
+    number++;
+}
+// for loop
+for(var i = 0; i <= 10; i++){
+    console.log(i);
+}
+
+// 5. Function
+function name(params) {
+    
+}
+
+// 6. object 
+var myMobil{
+    brand: 'oppo',
+    price: '14k'
+}
